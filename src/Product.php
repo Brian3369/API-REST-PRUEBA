ed75d7a33bc579a5764a7b10bb94c83391189139
Brian Alexander Tavarez Duarte <briam1952@gmail.com>
2025-04-11 16:12:09 -0400

    Agregar archivos de configuración y controlador de productos

---
+++ src/Product.php
@@ -0,0 +1,25 @@
+<?php
+require_once 'ProductController.php';
+
+$methoh = $_SERVER[['REQUEST_METHOD']];
+$controller = new ProductController();
+
+switch ($method) {
+    case 'GET':
+        $controller->getAllProducts();
+        break;
+    case 'POST':
+        $controller->createProduct();
+        break;
+    case 'PUT':
+        $controller->updateProduct();
+        break;
+    case 'DELETE':
+        $controller->deleteProduct();
+        break;
+    default:
+        http_response_code(405);
+        echo json_encode(['message' => 'Method Not Allowed']);
+        break;
+}
+?>
