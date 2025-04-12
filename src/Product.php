e1dc2d75f4166bda90cc17784cc815def2e4fe29
Brian Alexander Tavarez Duarte <briam1952@gmail.com>
2025-04-11 22:11:09 -0400

    Renombrar variables y métodos en Product y ProductController para mantener consistencia en español; implementar respuestas HTTP adecuadas en los métodos de creación y obtención de productos.

--- src/Product.php
+++ src/Product.php
@@ -1,27 +1,28 @@
 <?php
 class Product{
     private $conn;
-    private $table_name = "products";
+    private $table_name = "productos";
     public $id;
-    public $name;
-    public $description;
-    public $price;
+    public $nombre;
+    public $descripcion;
+    public $precio;
+    public $creado_en;
 
     public function __construct($db){
         $this->conn = $db;
     }
 
     public function createProduct(){
-        $stmt = $this->conn->prepare("INSERT INTO " . $this->table_name . " (name, description, price, creado_en) VALUES (:name, :description, :price, :creado_en)");
+        $query = "INSERT INTO " . $this->table_name . " SET nombre=:nombre, descripcion=:descripcion, precio=:precio, creado_en= NOW()";
+        $stmt = $this->conn->prepare($query);
 
-        $this->name = htmlspecialchars(strip_tags($this->name));
-        $this->description = htmlspecialchars(strip_tags($this->description));
-        $this->price = htmlspecialchars(strip_tags($this->price));
+        $this->nombre = htmlspecialchars(strip_tags($this->nombre));
+        $this->descripcion = htmlspecialchars(strip_tags($this->descripcion));
+        $this->precio = htmlspecialchars(strip_tags($this->precio));
 
-        $stmt->bindParam(':name', $this->name);
-        $stmt->bindParam(':description', $this->description);
-        $stmt->bindParam(':price', $this->price);
-        $stmt->bindParam(':creado_en', date('Y-m-d H:i:s'));
+        $stmt->bindParam(':nombre', $this->nombre);
+        $stmt->bindParam(':descripcion', $this->descripcion);
+        $stmt->bindParam(':precio', $this->precio);
 
         if($stmt->execute()){
             return true;
@@ -31,10 +32,35 @@ class Product{
     }
 
     public function getAllProducts(){
+        $query = "select * from " . $this->table_name;
+        $stmt = $this->conn->prepare($query);
 
+        if($stmt->execute()){
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }else{
+            return false;
+        }
     }
 
     public function updateProduct(){
+        $query = "update " . $this->table_name . " set nombre = :nombre, descripcion = :descripcion, precio = :precio where id=:id";
+        $stmt = $this->conn->prepare($query);
+
+        $this->id = htmlspecialchars(strip_tags($this->id));
+        $this->nombre = htmlspecialchars(strip_tags($this->nombre));
+        $this->descripcion = htmlspecialchars(strip_tags($this->descripcion));
+        $this->precio = htmlspecialchars(strip_tags($this->precio));
+
+        $stmt->bindParam(':id', $this->id);
+        $stmt->bindParam(':nombre', $this->nombre);
+        $stmt->bindParam(':descripcion', $this->descripcion);
+        $stmt->bindParam(':precio', $this->precio);
+
+        if($stmt->execute()){
+            return true;
+        }else{
+            return false;
+        }
 
     }
 
--- src/ProductController.php
+++ src/ProductController.php
@@ -12,15 +12,61 @@ class ProductController{
         $this->product = new Product($this->db);
     }
 
-    public function getAllProducts() {
+    public function createProduct(){
+        $data = json_decode(file_get_contents("php://input"));
 
+        if(isset($data->nombre) && isset($data->descripcion) && isset($data->precio)){
+            $this->product->nombre = $data->nombre;
+            $this->product->descripcion = $data->descripcion;
+            $this->product->precio = $data->precio;
+
+            if($this->product->createProduct()){
+                http_response_code(201);
+                echo json_encode(["message" => "Producto creado."]);
+
+            }else{
+                http_response_code(503);
+                echo json_encode(["message" => "Error al crear el producto."]);
+            }
+        }else{
+            http_response_code(400);
+            echo json_encode(["message" => "Datos incompletos."]);
+        }
     }
 
-    public function createProduct(){
+    public function getAllProducts() {
+        $data = $this->product->getAllProducts();
+        $registros = array();
+        $registros['registros'] = $data;
 
+        if($data){
+            http_response_code(200);
+            echo json_encode($registros);
+        }else{
+            http_response_code(404);
+            echo json_encode(["message" => "No se encontraron productos."]);
+        }
     }
 
     public function updateProduct(){
+        $data = json_decode(file_get_contents("php://input"));
+        if(isset($data->id) && isset($data->nombre) && isset($data->descripcion) && isset($data->precio)){
+            $this->product->id = $data->id;
+            $this->product->nombre = $data->nombre;
+            $this->product->descripcion = $data->descripcion;
+            $this->product->precio = $data->precio;
+
+            if($this->product->updateProduct()){
+                http_response_code(200);
+                echo json_encode(["message" => "Producto actualizado."]);
+            }else{
+                http_response_code(503);
+                echo json_encode(["message" => "Error al actualizar el producto."]);
+            }
+        }else{
+            http_response_code(400);
+            echo json_encode(["message" => "Datos incompletos."]);
+        }
 
     }
 
