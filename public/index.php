768d4d882c79330157bc8376636bd8c3753e1f74
Brian Alexander Tavarez Duarte <briam1952@gmail.com>
2025-04-12 02:00:28 -0400

    Modificar lógica de obtención de productos en index.php para usar datos JSON en lugar de parámetros GET; ajustar manejo de solicitudes OPTIONS.

--- public/index.php
+++ public/index.php
@@ -3,14 +3,23 @@ require_once '../src/ProductController.php';
 
 header('Content-Type: application/json; charset=UTF-8');
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 $method = $_SERVER['REQUEST_METHOD'];
 $controller = new ProductController();
 
 switch ($method) {
     case 'GET':
-        $id = isset($_GET['id']) ? $_GET['id'] : null;
-        if (!$id) {
+        $json = file_get_contents('php://input');
+        $data = json_decode($json, true);
+        
+        if ($json && isset($data['id'])) {
             $controller->getProductByID();
         } else {
             $controller->getAllProducts();
